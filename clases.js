04925fe9942d715b1b90fd0a5e72e9af6dd58dbc
Francisco De la O González <fran.delao@gmail.com>
2026-09-23 12:47:56 -0600

    Organizar Codigo
    Se ha separado clases, todo el script de app, y leve cambio al archivo style

---
+++ clases.js
@@ -0,0 +1,15 @@
+class Ingreso {
+    constructor(id, descripcion, valor) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.valor = valor;
+    }
+}
+ 
+class Egreso {
+    constructor(id, descripcion, valor) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.valor = valor;
+    }
+}
